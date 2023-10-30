0af082e03813471d9b12373dd15e4206e99a269f
amirparsaw <kalanpaeeamirparsa@gmail.com>
2023-10-30 08:11:52 +0330

    tst

---
+++ test.py
@@ -0,0 +1,8 @@
+# Step 1: Define the input value in kilometers
+kilometers = 5
+
+# Step 2: Multiply kilometers by 1000 to convert to meters
+meters = kilometers * 1000
+
+# Step 3: Display the result
+print(f"{kilometers} kilometers is equal to {meters} meters")
